bdffb233afb637e5d874a35bd9ed8cdcd5f45611
saiseran-s <s.saiseran@gmail.com>
2018-02-19 14:40:50 +0530

    sai

--- firesafty2.c
+++ firesafty2.c
@@ -1,16 +1,16 @@
 /*
 AVR ATmega8
 Description : This code is based on ATmega8 and integreated with WIFI esp8266, mq135, flame sensor and lm35.
-Author :- S Sai Seran
+Author    :- S Sai Seran
 Co-Author :-M Pavan Sai
 */
 #include <avr/io.h>
 #include <util/delay.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <compat/deprecated.h>
-#define F_CPU 16000000
 
 uint16_t readAnalag(uint8_t num)
 {
@@ -29,7 +29,8 @@ uint16_t readAnalag(uint8_t num)
 }
 #define dl _delay_ms(3000)
 
-char x[32],y[32],z[32];
+unsigned char x[32],y[32],z[32];
+unsigned char GET[57]="GET /firesafty/requestData/request.php?field=M1&field1=",f2[9]="&field2=",f3[9]="&field3=";
 
 void usart_init() {
   // Set baud rate
@@ -61,52 +62,61 @@ void UART_Tstring(char *p)
 int main(void)
 {
   
-  char data[5];
-  usart_init ();          /*initiate usart*/
-  _delay_us(3000);
-  UART_Tstring("AT\r\n");     /*check wifi response*/
-  _delay_ms(2000);
-  
-  _delay_ms(2000);
-  UART_Tstring("ATE0\r\n");
-  dl;
-  UART_Tstring("AT+CWMODE=1\r\n");/*Set mode*/      
-  _delay_ms(3000);
-  UART_Tstring("AT+CWJAP=\"Drago\",\"drago1234\"\r\n");/*connect to wifi*/
-  _delay_ms(5000);
-  dl;
-  UART_Tstring("AT+CIPMUX=0\r\n");/*multiplexing*/
-  _delay_ms(4000);
+    char data[5];
+    usart_init ();          /*initiate usart*/
+    _delay_us(3000);
+    UART_Tstring("AT\r\n");     /*check wifi response*/
+    _delay_ms(2000);
+    
+    _delay_ms(2000);
+    UART_Tstring("ATE0\r\n");
+    dl;
+    UART_Tstring("AT+CWMODE=1\r\n");/*Set mode*/      
+    _delay_ms(3000);
+    UART_Tstring("AT+CWJAP=\"Drago\",\"drago1234\"\r\n");/*connect to wifi*/
+    _delay_ms(5000);
+    dl;
+    UART_Tstring("AT+CIPMUX=0\r\n");/*multiplexing*/
+    _delay_ms(4000);
   
   while(1)
-  {
-    
-    
-    _delay_ms(4000);
-    UART_Tstring("AT+CIPSTART=\"TCP\",\"acet.ac.in\",80\r\n");
-    _delay_ms(6000);
-    _delay_ms(5000);
-    UART_Tstring("GET /firesafty/requestData/request.php?field=M1&field1=");
-    int a= readAnalag(0);
-    float mv = ((a/1024)*5000);
-    int cel_i = mv/10;
-    int cel_d = (mv*10) - (cel_i*100)
-    itoa(cel_i,x,10);
-    UART_Tstring(x);
-    UART_Tstring(".");
-    UART_Tstring(cel_d);
-    _delay_ms(2000);
-    UART_Tstring("&field2=");
-    _delay_us(2000);
-    uint8_t b= readAnalag(3);
-    itoa(b,y,10);
-    UART_Tstring(y);
-    UART_Tstring("&field3=");
-    _delay_us(2000);
-    uint8_t c=readAnalag(5);
-    itoa(c,z,10);
-    UART_Tstring(z);
-    UART_Tstring("\r\n");
-    _delay_ms(9000);
+  {  
+  
+  UART_Tstring("AT+CIPSTART=\"TCP\",\"acet.ac.in\",80\r\n");
+  _delay_ms(4000);
+   UART_Tstring("AT+CIPSEND=87\r\n");
+  _delay_ms(3000);
+  UART_Tstring(GET);
+  _delay_us(100);
+
+  uint8_t a = readAnalag(0);
+  float mv = {(a/1024.0)*5000};
+  uint16_t cel_i = mv/10;
+  uint16_t cel_d = [(mv*10.0) - (cel_i*100)];
+  itoa(cel_i,x,10);
+  UART_Tstring(x);
+  UART_Tstring(".");
+  itoa(cel_d,x,10);
+  UART_Tstring(x);
+  _delay_ms(200);
+
+
+  UART_Tstring(f2);
+  _delay_us(200);
+  uint16_t b = readAnalag(3);
+  itoa(b,y,10);
+  _delay_us(200);
+  UART_Tstring(y);
+  _delay_us(100);
+  UART_Tstring(f3);
+  _delay_us(200);
+  uint16_t c = readAnalag(5);
+  itoa(c,z,10);
+  UART_Tstring(z);
+  UART_Tstring("\r\n");
+  _delay_ms(4000);
+  UART_Tstring("AT+CIPCLOSE");
+  _delay_ms(2000);
+
   }
 }
