2f0605a46ed0151032bc2cafd3704e9bb607de2d
pavanmaddula <31445693+pavanmaddula@users.noreply.github.com>
2018-03-06 09:46:20 +0530

    Update firesafty2.c

--- firesafty2.c
+++ firesafty2.c
@@ -34,7 +34,7 @@ uint16_t readAnalag(uint8_t num)
 #define dl _delay_ms(3000)
 
 unsigned char x[32],y[32],z[32];
-unsigned char GET[57]="GET /firesafty/requestData/request.php?field=M1&field1=",f2[9]="&field2=",f3[9]="&field3=";
+unsigned char GET[58]="GET /firesafty/requestData/request.php?field=M1&field1=",f2[9]="&field2=",f3[9]="&field3=";
 
 void usart_init() {
   // Set baud rate
@@ -72,7 +72,6 @@ int main(void)
     UART_Tstring("AT\r\n");     /*check wifi response*/
     _delay_ms(2000);
     
-<<<<<<< HEAD
     _delay_ms(2000);
     UART_Tstring("ATE0\r\n");
     dl;
@@ -93,60 +92,31 @@ int main(void)
   _delay_ms(5000);
   UART_Tstring(GET);
   _delay_us(1000);
-  uint8_t a = readAnalag(5);
-  float mv = (5*a*100/1024.0); 
-  uint16_t cel_i = mv/10;
-  uint16_t cel_d = [(mv*10.0) - (cel_i*100)];
+   UART_Tstring(x);
+  uint16_t a = readAnalag(5);
+  uint64_t k = ( a/1024.0)*5000; 
+  uint16_t cel_i = k/10;
   itoa(cel_i,x,10);
-  UART_Tstring(x);
-  UART_Tstring(".");
-  itoa(cel_d,x,10);
-  UART_Tstring(x);
   _delay_ms(200);
   UART_Tstring(f2);
   _delay_us(2000);
-  uint16_t b = readAnalag(4);
+  uint16_t b = readAnalag(1);
   itoa(b,y,10);
   _delay_us(200);
   UART_Tstring(y);
   _delay_us(100);
   UART_Tstring(f3);
   _delay_us(2000);
-  uint16_t c = readAnalag(1);
+  uint16_t c = readAnalag(2);
   itoa(c,z,10);
   UART_Tstring(z);
   UART_Tstring("\r\n");
   _delay_ms(6000);
-
-=======
-    _delay_ms(4000);
-    UART_Tstring("AT+CIPSTART=\"TCP\",\"acet.ac.in\",80\r\n");
-    _delay_ms(6000);
-    _delay_ms(5000);
-    UART_Tstring("GET /firesafty/requestData/request.php?field=M1&field1=");
-    int a= readAnalag(0);
-    float mv = ((a/1024)*5000);
-    int cel_i = mv/10;
-    int cel_d = (mv*10) - (cel_i*100);
-    itoa(cel_i,x,10);
-    UART_Tstring(x);
-    UART_Tstring(".");
-    UART_Tstring(cel_d);
-    _delay_ms(2000);
-    UART_Tstring("&field2=");
-    _delay_us(2000);
-    uint8_t b= readAnalag(3);
-    itoa(b,y,10);
-    UART_Tstring(y);
-    UART_Tstring("&field3=");
-    _delay_us(2000);
-    uint8_t c=readAnalag(5);
-    itoa(c,z,10);
-    UART_Tstring(z);
-    UART_Tstring("\r\n");
-    _delay_ms(4000);
-    UART_TSTRING("AT+CIPCLOSE\r\n");
+  UART_Tstring("AT+CIPCLOSE\r\n");
+  _delay_ms(4000);
+  
+  
+ 
     
->>>>>>> b2bf74dd18901689ba0386500ff2ac9e89943407
   }
 }
